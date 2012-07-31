c10d8476d35a2dde4f8c358c640dd69085b01e4d
LucasJones <lucas@frizfraz.com>
2012-07-31 14:06:01 +0200

    Code Cleanup

--- ee3_client/ee3/client/core/MinumInWaterHandler.java
+++ ee3_client/ee3/client/core/MinumInWaterHandler.java
@@ -11,19 +11,18 @@ public class MinumInWaterHandler {
 	private static Minecraft mc = ModLoader.getMinecraftInstance();
 	public static void HandleItems(){
 		if(!mc.theWorld.isRemote){
-		for(int i = 0; i < mc.theWorld.loadedEntityList.size(); i++) {
-			Entity entity = (Entity)mc.theWorld.loadedEntityList.get(i);
-			if(entity instanceof EntityItem){
-				EntityItem item = (EntityItem)entity;
-				if(item.item.getItem().shiftedIndex == ModItems.miniumShard.shiftedIndex){
-					//If the helper has replaced the water with red water
-					if(Helper.handleWaterMovement((EntityItem)entity)){
-						//Destroy the minum shard
-						entity.setDead();
+			for(int i = 0; i < mc.theWorld.loadedEntityList.size(); i++) {
+				Entity entity = (Entity)mc.theWorld.loadedEntityList.get(i);
+				if(entity instanceof EntityItem){
+					if(((EntityItem)entity).item.getItem().shiftedIndex == ModItems.miniumShard.shiftedIndex){
+						//If the helper has replaced the water with red water
+						if(Helper.handleWaterMovement((EntityItem)entity)){
+							//Destroy the minum shard
+							entity.setDead();
+						}
 					}
 				}
 			}
-		}
 		}
 	}
 }
