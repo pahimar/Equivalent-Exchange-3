a106e451b9f955f14a5823eaf472217aa08748df
pahimar <pahimar@gmail.com>
2012-08-15 14:23:58 -0400

    Initial commit for MC 1.3.* work. Starting over for cleanliness (and less temptation for hacky hacky)

---
+++ ee3_common/ee3/network/PacketHandler.java
@@ -0,0 +1,17 @@
+package ee3.network;
+
+import net.minecraft.src.NetworkManager;
+import net.minecraft.src.Packet250CustomPayload;
+import cpw.mods.fml.common.network.IPacketHandler;
+import cpw.mods.fml.common.network.Player;
+
+public class PacketHandler implements IPacketHandler {
+
+	@Override
+	public void onPacketData(NetworkManager manager,
+			Packet250CustomPayload packet, Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
