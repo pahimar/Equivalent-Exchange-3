3d4fc7c88517ba2a620f9559ff9d9e75aa430bcd
pahimar <rob.a.r.davis@gmail.com>
2012-08-16 21:30:56 -0400

    Setting up the mcmod file again

--- ee3_common/ee3/lib/Reference.java
+++ ee3_common/ee3/lib/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MOD_ID = "EE3";
 	public static final String MOD_NAME = "Equivalent Exchange 3";
-	public static final String VERSION = "1.0.0.1";
+	public static final String VERSION = "1.0.0.0";
 	public static final String CHANNEL_NAME = MOD_ID;
 }
