ea92584788eb670a30afc5f5a32f2bb41ac16736
pahimar <pahimar@gmail.com>
2012-09-28 11:19:13 -0400

    Adding a bunch of user contributed localizations (thanks guys!)

--- ee3_common/ee3/common/lib/Localizations.java
+++ ee3_common/ee3/common/lib/Localizations.java
@@ -15,8 +15,11 @@ public class Localizations {
 	
 	public static String[] localeFiles = {
 		LANG_RESOURCE_LOCATION + "cy_GB.xml",
+		LANG_RESOURCE_LOCATION + "de_DE.xml",
 		LANG_RESOURCE_LOCATION + "en_US.xml",
-		LANG_RESOURCE_LOCATION + "nl_NL.xml"
+		LANG_RESOURCE_LOCATION + "it_IT.xml",
+		LANG_RESOURCE_LOCATION + "nl_NL.xml",
+		LANG_RESOURCE_LOCATION + "ru_RU.xml"
 	};
 
 }
