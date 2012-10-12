e851355a343301febd2e407ef09f7b40c77d1499
RedRoserade <afecarvalho@gmail.com>
2012-10-12 21:28:05 +0200

    Update ee3_common/ee3/common/lib/Localizations.java

--- ee3_common/ee3/common/lib/Localizations.java
+++ ee3_common/ee3/common/lib/Localizations.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-package ee3.common.lib;
-
-/**
- * BlockIds
- * 
- * Library containing all mod language related files
- * 
- * @author pahimar
- * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- * 
- */
-public class Localizations {
-	
-	private static final String LANG_RESOURCE_LOCATION = "/ee3/lang/";
-	
-	public static String[] localeFiles = {
-		LANG_RESOURCE_LOCATION + "en_US.xml",
-		LANG_RESOURCE_LOCATION + "nl_NL.xml",
-		LANG_RESOURCE_LOCATION + "pt_pT.xml"
-	};
-
-}
-=======
 package ee3.common.lib;
 
 /**
@@ -48,4 +24,3 @@ public class Localizations {
 	};
 
 }
->>>>>>> e302df5b9564404600123b40e8531cf63d2e202b
