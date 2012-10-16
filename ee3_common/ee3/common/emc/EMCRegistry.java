e666b0f5460e55095545e5b1d1d031090a2d43a3
pahimar <pahimar@gmail.com>
2012-10-16 10:38:31 -0400

    Fix some whitespace issues, and some renames for EMC types

--- ee3_common/ee3/common/emc/EMCRegistry.java
+++ ee3_common/ee3/common/emc/EMCRegistry.java
@@ -8,16 +8,16 @@ import net.minecraft.src.ItemStack;
 public class EMCRegistry {
 
 	private static final EMCRegistry emcRegistry = new EMCRegistry();
-	
+
 	private HashMap<Integer, HashMap<Integer, EMCEntry>> emcMap = new HashMap<Integer, HashMap<Integer, EMCEntry>>();
-	
+
 	public EMCRegistry instance() {
 		return emcRegistry;
 	}
-	
+
 	public EMCEntry getEMCEntry(ItemStack item) {
-		
+
 		return null;
 	}
-	
+
 }
