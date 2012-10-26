4b29fb2b0b1639c8e3e5be8eea66c42990ab745c
pahimar <pahimar@gmail.com>
2012-10-26 10:32:35 -0400

    Added some of the new localizations to the listing of files, and corrected the comments (it's Localizations, not BlockIds....)

--- ee3_common/ee3/common/lib/Localizations.java
+++ ee3_common/ee3/common/lib/Localizations.java
@@ -1,7 +1,7 @@
 package ee3.common.lib;
 
 /**
- * BlockIds
+ * Localizations
  * 
  * Library containing all mod language related files
  * 
@@ -14,15 +14,17 @@ public class Localizations {
 	private static final String LANG_RESOURCE_LOCATION = "/ee3/lang/";
 	
 	public static String[] localeFiles = {
+		LANG_RESOURCE_LOCATION + "cs_CZ.xml",
 		LANG_RESOURCE_LOCATION + "cy_GB.xml",
 		LANG_RESOURCE_LOCATION + "de_DE.xml",
 		LANG_RESOURCE_LOCATION + "en_US.xml",
+		LANG_RESOURCE_LOCATION + "es_ES.xml",
 		LANG_RESOURCE_LOCATION + "fr_FR.xml",
 		LANG_RESOURCE_LOCATION + "it_IT.xml",
 		LANG_RESOURCE_LOCATION + "nl_NL.xml",
 		LANG_RESOURCE_LOCATION + "pt_PT.xml",
-		LANG_RESOURCE_LOCATION + "es_ES.xml",
-		LANG_RESOURCE_LOCATION + "ru_RU.xml"
+		LANG_RESOURCE_LOCATION + "ru_RU.xml",
+		LANG_RESOURCE_LOCATION + "sv_SE.xml"
 	};
 
 }
