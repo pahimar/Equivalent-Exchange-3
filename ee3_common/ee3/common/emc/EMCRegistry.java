a333b5138dc589bd7ee10632661456a887dc7adb
UberWaffe <UberWaffe@gmail.com>
2012-11-07 11:24:24 +0200

    Added comments.

--- ee3_common/ee3/common/emc/EMCRegistry.java
+++ ee3_common/ee3/common/emc/EMCRegistry.java
@@ -11,6 +11,9 @@ import net.minecraft.src.ItemStack;
  * EMCRegistry
  * 
  * Registry containing all EMC entries for objects
+ * Essentially, a big list in which you can look up the EMC values (of each EMC type) for any object.
+ * Meaning, you give it an ID of a block or item, and it gives you the EMCEntry (list of EMC values of each type) for it.
+ * One such registry (instance of this class) should exist per server.
  * 
  * @author pahimar
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
@@ -20,6 +23,10 @@ public class EMCRegistry {
 
 	private static final EMCRegistry emcRegistry = new EMCRegistry();
 
+	/**
+	 * This keep one EMCEntry record per item/block ID.
+	 * emcMap: <Block/Item ID, <Block/Item Meta-ID, EMCEntry>>
+	 */
 	private HashMap<Integer, HashMap<Integer, EMCEntry>> emcMap = new HashMap<Integer, HashMap<Integer, EMCEntry>>();
 
 	public static EMCRegistry instance() {
@@ -54,6 +61,11 @@ public class EMCRegistry {
 		return getEMCValue(id, 0);
 	}
 	
+	/**
+	 * Get EMC Values of an id (block/item) with meta-parts.
+	 * Ex: Saplings have the same ID, but different meta (or sub-id's).
+	 * In the above example ID would say it is a sapling, while meta would say what kind.
+	 */
 	public EMCEntry getEMCValue(int id, int meta) {
 		if (emcMap.containsKey(id)) {
 			if (emcMap.get(id).containsKey(meta)) {
@@ -84,6 +96,10 @@ public class EMCRegistry {
 		addEMCValue(id, 0, emcEntry);
 	}
 	
+	/**
+	 * Adds a new EMCEntry for the id into the registry.
+	 * Note: If the id (+meta) already exists, then it will do nothing.
+	 */
 	public void addEMCValue(int id, int meta, EMCEntry emcEntry) {
 		HashMap<Integer, EMCEntry> tempMap = new HashMap<Integer, EMCEntry>();
 		
