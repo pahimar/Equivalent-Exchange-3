01304fb4c45b8926788e6756a28a29ac8fd51b3a
pahimar <pahimar@gmail.com>
2012-11-20 14:42:39 -0500

    Refactored the remote version check to check against the GitHub repo instead of my DropBox folder, added some colourized text to the result message on the client, and reformatted some of the localization.

--- ee3_common/ee3/common/lib/Colours.java
+++ ee3_common/ee3/common/lib/Colours.java
@@ -14,6 +14,8 @@ public class Colours {
     public static final String PURE_RED = "ff0000";
 
     /* Text colour related constants */
-    public static final String VERSION_CHECK_PREFIX = "\u00a7e";
+    public static final String TEXT_COLOUR_PREFIX_YELLOW = "\u00a7e";
+
+    public static final String TEXT_COLOUR_PREFIX_WHITE = "\u00a7f";
     
 }
