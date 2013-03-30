62ad61134426c8344baf82ab1cfd78e0198583cf
Ross Kenneth Keslo <rosskelso@btinternet.com>
2013-03-30 20:36:33 +0000

    Added iron to copper tansmutation

---
+++ ee3_common/com/pahimar/ee3/core/addons/CopperTransmute.java
@@ -0,0 +1,27 @@
+package com.pahimar.ee3.core.addons;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
+import com.pahimar.ee3.core.helper.RecipeHelper;
+
+import java.util.ArrayList;
+
+
+public class CopperTransmute
+{
+    
+    public static void copper(ItemStack transmutationStone)
+    {   
+        ArrayList<ItemStack > copperOres = OreDictionary.getOres("copper");
+        ItemStack copperItem = copperOres.get(0);
+        try
+        {
+            RecipeHelper.addRecipe(copperItem, transmutationStone , Item.ingotIron, Item.ingotIron);
+        }
+        catch (Exception e)
+        {
+            //swallowed exception so ignored if copper not present
+        }
+    }
+}
