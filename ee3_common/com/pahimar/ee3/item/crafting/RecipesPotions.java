b56edebe9a980fc85e58d861a5d97075782e9357
pahimar <pahimar@gmail.com>
2013-06-24 00:04:43 -0400

    Some work on how to determine brewing relationships

---
+++ ee3_common/com/pahimar/ee3/item/crafting/RecipesPotions.java
@@ -0,0 +1,30 @@
+package com.pahimar.ee3.item.crafting;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+
+public class RecipesPotions {
+    
+    public static ItemStack bottleWater = new ItemStack(Item.potion.itemID, 1, 0);
+    public static ItemStack potionAwkward = new ItemStack(Item.potion.itemID, 1, 16);
+    public static ItemStack potionThick = new ItemStack(Item.potion.itemID, 1, 32);
+    public static ItemStack potionMundaneBase = new ItemStack(Item.potion.itemID, 1, 64);
+    public static ItemStack potionMundaneExtended = new ItemStack(Item.potion.itemID, 1, 128);
+    
+    public static ItemStack potionRegeneration = new ItemStack(Item.potion.itemID, 1, 8193);
+    public static ItemStack potionRegenerationExtended = new ItemStack(Item.potion.itemID, 1, 8257);
+    public static ItemStack potionRegenerationEnhanced = new ItemStack(Item.potion.itemID, 1, 8225);
+    
+    public static ItemStack potionSwiftness = new ItemStack(Item.potion.itemID, 1, 8194);
+    public static ItemStack potionSwiftnessExtended = new ItemStack(Item.potion.itemID, 1, 8258);
+    public static ItemStack potionSwiftnessEnhanced = new ItemStack(Item.potion.itemID, 1, 8226);
+    
+    public static ItemStack potionFireResist = new ItemStack(Item.potion.itemID, 1, 8195);
+    public static ItemStack potionFireResistExtended = new ItemStack(Item.potion.itemID, 1, 8259);
+    public static ItemStack potionFireResistEnhanced = new ItemStack(Item.potion.itemID, 1, 8227);
+    
+    public static ItemStack potionHealing = new ItemStack(Item.potion.itemID, 1, 8197);
+    public static ItemStack potionHealingExtended = new ItemStack(Item.potion.itemID, 1, 8261);
+    public static ItemStack potionHealingEnhanced = new ItemStack(Item.potion.itemID, 1, 8229);
+}
