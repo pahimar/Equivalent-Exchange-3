1109bf31db8fb5b61ce7f42a25e8daabe009dda1
pahimar <pahimar@gmail.com>
2013-11-26 23:22:40 -0500

    Removing the unnecessary EmcBlacklist, and some work on IMC

--- common/com/pahimar/ee3/imc/InterModCommsHandler.java
+++ common/com/pahimar/ee3/imc/InterModCommsHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.minecraft.item.ItemStack;
 
 import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
 import com.pahimar.ee3.EquivalentExchange3;
 import com.pahimar.ee3.api.RecipeMapping;
 import com.pahimar.ee3.api.StackValueMapping;
@@ -25,6 +26,8 @@ import cpw.mods.fml.common.event.FMLInterModComms.IMCMessage;
 
 public class InterModCommsHandler implements ITickHandler, IScheduledTickHandler{
 
+    private static Gson gson = new Gson();
+    
     // TODO Logging
     
     public static void processIMCMessages(IMCEvent event) {
@@ -40,12 +43,6 @@ public class InterModCommsHandler implements ITickHandler, IScheduledTickHandler
 
         if (requestedOperation.equalsIgnoreCase(InterModCommsOperations.RECIPE_ADD)) {
             processAddRecipeMessage(imcMessage);
-        }
-        else if (requestedOperation.equalsIgnoreCase(InterModCommsOperations.BLACKLIST_ADD_ENTRY)) {
-            processAddBlackListMessage(imcMessage);
-        }
-        else if (requestedOperation.equalsIgnoreCase(InterModCommsOperations.BLACKLIST_REMOVE_ENTRY)) {
-            processRemoveBlackListMessage(imcMessage);
         }
         else if (requestedOperation.equalsIgnoreCase(InterModCommsOperations.EMC_ASSIGN_VALUE_PRE)) {
             processPreAssignEmcValueMessage(imcMessage);
@@ -81,16 +78,6 @@ public class InterModCommsHandler implements ITickHandler, IScheduledTickHandler
         else {
             // TODO Log that the message payload is of an invalid type
         }
-    }
-
-    private static void processAddBlackListMessage(IMCMessage imcMessage) {
-
-        // TODO
-    }
-
-    private static void processRemoveBlackListMessage(IMCMessage imcMessage) {
-
-        // TODO
     }
 
     private static void processPreAssignEmcValueMessage(IMCMessage imcMessage) {
@@ -141,11 +128,14 @@ public class InterModCommsHandler implements ITickHandler, IScheduledTickHandler
 
         if (imcMessage.getMessageType() == String.class) {
             
-            // TODO What if it is an encoded ItemStack | OreStack | EnergyStack | FluidStack
             WrappedStack wrappedStack = WrappedStack.createFromJson(imcMessage.getStringValue());
             
             if (wrappedStack != null) {
-                
+                FMLInterModComms.sendRuntimeMessage(
+                        EquivalentExchange3.instance, 
+                        imcMessage.getSender(), 
+                        InterModCommsOperations.EMC_RETURN_HAS_VALUE, 
+                        String.format("%s==%s", imcMessage.getStringValue(), String.valueOf(EmcRegistry.hasEmcValue(wrappedStack))));
             }
             else {
                 // TODO Log that the message payloads json was invalid
@@ -153,10 +143,18 @@ public class InterModCommsHandler implements ITickHandler, IScheduledTickHandler
         }
         else if (imcMessage.getMessageType() == ItemStack.class) {
             
-            /*
-             * Reply back to the mod that queried for the existance of an EmcValue for the given ItemStack
-             */
-            FMLInterModComms.sendRuntimeMessage(EquivalentExchange3.instance, imcMessage.getSender(), InterModCommsOperations.EMC_RETURN_HAS_VALUE, String.valueOf(EmcRegistry.hasEmcValue(imcMessage.getItemStackValue())));
+            ItemStack itemStack = imcMessage.getItemStackValue();
+
+            if (itemStack != null) {
+                FMLInterModComms.sendRuntimeMessage(
+                        EquivalentExchange3.instance, 
+                        imcMessage.getSender(), 
+                        InterModCommsOperations.EMC_RETURN_HAS_VALUE, 
+                        String.format("%s==%s", gson.toJson(itemStack), String.valueOf(EmcRegistry.hasEmcValue(itemStack))));
+            }
+            else {
+                // TODO Logging
+            }
         }
         else {
             // TODO Log that the message payload is of an invalid type
@@ -167,11 +165,14 @@ public class InterModCommsHandler implements ITickHandler, IScheduledTickHandler
         
         if (imcMessage.getMessageType() == String.class) {
             
-            // TODO What if it is an encoded ItemStack | OreStack | EnergyStack | FluidStack
             WrappedStack wrappedStack = WrappedStack.createFromJson(imcMessage.getStringValue());
             
             if (wrappedStack != null) {
-                
+                FMLInterModComms.sendRuntimeMessage(
+                        EquivalentExchange3.instance, 
+                        imcMessage.getSender(), 
+                        InterModCommsOperations.EMC_RETURN_GET_VALUE, 
+                        String.format("%s==%s", imcMessage.getStringValue(), EmcRegistry.getEmcValue(wrappedStack).toJson()));
             }
             else {
                 // TODO Log that the message payloads json was invalid
@@ -182,14 +183,14 @@ public class InterModCommsHandler implements ITickHandler, IScheduledTickHandler
             /*
              * Reply back to the mod that queried for the existance of an EmcValue for the given ItemStack
              */
-            EmcValue emcValue = EmcRegistry.getEmcValue(imcMessage.getItemStackValue());
+            ItemStack itemStack = imcMessage.getItemStackValue();
             
-            if (emcValue != null) {
-                FMLInterModComms.sendRuntimeMessage(EquivalentExchange3.instance, imcMessage.getSender(), InterModCommsOperations.EMC_RETURN_GET_VALUE, emcValue.toJson());
-            }
-            else {
-                FMLInterModComms.sendRuntimeMessage(EquivalentExchange3.instance, imcMessage.getSender(), InterModCommsOperations.EMC_RETURN_GET_VALUE, "null");
-            }
+            FMLInterModComms.sendRuntimeMessage(
+                    EquivalentExchange3.instance, 
+                    imcMessage.getSender(), 
+                    InterModCommsOperations.EMC_RETURN_GET_VALUE, 
+                    String.format("%s==%s", gson.toJson(itemStack), EmcRegistry.getEmcValue(itemStack).toJson()));
+
         }
         else {
             // TODO Log that the message payload is of an invalid type
--- common/com/pahimar/ee3/imc/InterModCommsOperations.java
+++ common/com/pahimar/ee3/imc/InterModCommsOperations.java
@@ -5,10 +5,6 @@ public class InterModCommsOperations {
     // Interacting with the Recipe Registry
     public static final String RECIPE_ADD = "recipe-add";
 
-    // Interacting with the EMC BlackList
-    public static final String BLACKLIST_ADD_ENTRY = "blacklist-add-entry";
-    public static final String BLACKLIST_REMOVE_ENTRY = "blacklist-remove-entry";
-
     // Interacting with the EMC value mappings
     public static final String EMC_ASSIGN_VALUE_PRE = "emc-assign-value-pre";
     public static final String EMC_ASSIGN_VALUE_POST = "emc-assign-value-post";
