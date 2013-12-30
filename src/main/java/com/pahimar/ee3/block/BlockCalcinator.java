ed463d38c21faf64bb90edd74abca79676b3a1a4
pahimar <pahimar@gmail.com>
2013-12-29 22:44:06 -0500

    Some simple particle effects for the fire pot on the calcinator

--- src/main/java/com/pahimar/ee3/block/BlockCalcinator.java
+++ src/main/java/com/pahimar/ee3/block/BlockCalcinator.java
@@ -2,6 +2,7 @@ package com.pahimar.ee3.block;
 
 import com.pahimar.ee3.EquivalentExchange3;
 import com.pahimar.ee3.lib.GuiIds;
+import com.pahimar.ee3.lib.Particles;
 import com.pahimar.ee3.lib.RenderIds;
 import com.pahimar.ee3.lib.Strings;
 import com.pahimar.ee3.tileentity.TileCalcinator;
@@ -103,7 +104,18 @@ public class BlockCalcinator extends BlockContainerEE
     @Override
     public void randomDisplayTick(World world, int x, int y, int z, Random random)
     {
-        // TODO Spawn particles
+        if (world.getBlockTileEntity(x, y, z) instanceof TileCalcinator)
+        {
+            if (((TileCalcinator) world.getBlockTileEntity(x, y, z)).isBurning())
+            {
+                // Fire pot particles
+                world.spawnParticle(Particles.NORMAL_SMOKE, (double) x + 0.5F, (double) y + 0.4F, (double) ((z + 0.5F) + (random.nextFloat() * 0.5F - 0.3F)), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle(Particles.FLAME, (double) x + 0.5F, (double) y + 0.4F, (double) z + 0.5F, 0.0D, 0.0D, 0.0D);
+
+                // Bowl particle effects
+                // TODO Decide if the bowl should have particle effects
+            }
+        }
     }
 
     private void dropInventory(World world, int x, int y, int z)
