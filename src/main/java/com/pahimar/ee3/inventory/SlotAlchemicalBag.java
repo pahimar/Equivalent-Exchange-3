b8d6a9fb45d027567a429f213145848a17336c48
pahimar <pahimar@gmail.com>
2014-03-26 21:26:46 -0400

    You can not store bags inside of bags!

--- src/main/java/com/pahimar/ee3/inventory/SlotAlchemicalBag.java
+++ src/main/java/com/pahimar/ee3/inventory/SlotAlchemicalBag.java
@@ -1,5 +1,6 @@
 package com.pahimar.ee3.inventory;
 
+import com.pahimar.ee3.item.ItemAlchemicalBag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
@@ -22,5 +23,14 @@ public class SlotAlchemicalBag extends Slot
     {
         super.onSlotChange(itemStack1, itemStack2);
         containerAlchemicalBag.saveInventory(entityPlayer);
+    }
+
+    /**
+     * Check if the stack is a valid item for this slot. Always true beside for the armor slots.
+     */
+    @Override
+    public boolean isItemValid(ItemStack itemStack)
+    {
+        return itemStack.getItem() instanceof ItemAlchemicalBag ? false : true;
     }
 }
