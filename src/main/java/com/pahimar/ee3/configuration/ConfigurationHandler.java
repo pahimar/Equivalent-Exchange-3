406542dd3d37d000218dc6b6578dbf74089d3861
Robert R <rreleford97@gmail.com>
2014-06-03 21:59:46 -0400

    Fixed serverside instance of ClientConfig fixes #681

--- src/main/java/com/pahimar/ee3/configuration/ConfigurationHandler.java
+++ src/main/java/com/pahimar/ee3/configuration/ConfigurationHandler.java
@@ -1,11 +1,14 @@
 package com.pahimar.ee3.configuration;
 
+import com.pahimar.ee3.EquivalentExchange3;
+import com.pahimar.ee3.proxy.ClientProxy;
 import java.io.File;
 
 public class ConfigurationHandler
 {
     public static void init(String configPath)
     {
-        ClientConfiguration.init(new File(configPath + "client.properties"));
+	if(EquivalentExchange3.proxy instanceof ClientProxy)
+        	ClientConfiguration.init(new File(configPath + "client.properties"));
     }
 }
