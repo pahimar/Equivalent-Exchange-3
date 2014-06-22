5bfe76336cdcf5d54ba8be6b63115509f628c26d
Pahimar <pahimar@gmail.com>
2014-06-22 14:03:30 -0400

    Add in some more testing with the Player load/save events (discovered a possible bug with the event for the loading of player data in a single player world)

--- src/main/java/com/pahimar/ee3/handler/PlayerEventHandler.java
+++ src/main/java/com/pahimar/ee3/handler/PlayerEventHandler.java
@@ -4,17 +4,43 @@ import com.pahimar.ee3.util.LogHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
+import java.io.File;
+import java.io.IOException;
+
 public class PlayerEventHandler
 {
     @SubscribeEvent
     public void onPlayerSaveToFileEvent(PlayerEvent.SaveToFile event)
     {
         LogHelper.info("Save Event: " + event.playerUUID);
+        File playerFile = event.getPlayerFile("ee3");
+        if (!playerFile.exists())
+        {
+            LogHelper.info("Creating knowledge file for player with UUID: " + event.playerUUID);
+            try
+            {
+                playerFile.createNewFile();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     @SubscribeEvent
     public void onPlayerLoadFromFileEvent(PlayerEvent.LoadFromFile event)
     {
         LogHelper.info("Load Event: " + event.playerUUID);
+
+        File playerFile = event.getPlayerFile("ee3");
+        try
+        {
+            playerFile.createNewFile();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
