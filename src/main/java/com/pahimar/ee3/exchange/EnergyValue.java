3c69ee4f7b25c317b4e2a98d5ffd0aa6ce3430a4
Pahimar <pahimar@gmail.com>
2014-07-10 16:20:35 -0400

    Moving things around

--- src/main/java/com/pahimar/ee3/exchange/EnergyValue.java
+++ src/main/java/com/pahimar/ee3/exchange/EnergyValue.java
@@ -72,8 +72,6 @@ public class EnergyValue implements Comparable<EnergyValue>
     {
         OMNI, CORPOREAL, KINETIC, TEMPORAL, ESSENTIA, AMORPHOUS, VOID;
 
-        public static final EnergyType[] TYPES = EnergyType.values();
-
         public static final EnergyType DEFAULT = EnergyType.CORPOREAL;
     }
 }
