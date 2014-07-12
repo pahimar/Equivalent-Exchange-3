d497d3f9d90840c1803cce0dc44f6cff70c059fa
pahimar <pahimar@gmail.com>
2014-07-12 19:10:04 -0400

    Smarter localization of Creative Tabs

--- src/main/java/com/pahimar/ee3/creativetab/CreativeTab.java
+++ src/main/java/com/pahimar/ee3/creativetab/CreativeTab.java
@@ -2,27 +2,17 @@ package com.pahimar.ee3.creativetab;
 
 import com.pahimar.ee3.init.ModItems;
 import com.pahimar.ee3.reference.Reference;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.util.StatCollector;
 
 public class CreativeTab
 {
-    public static final CreativeTabs EE3_TAB = new CreativeTabs(Reference.MOD_ID)
+    public static final CreativeTabs EE3_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase())
     {
         @Override
         public Item getTabIconItem()
         {
             return ModItems.stonePhilosophers;
-        }
-
-        @Override
-        @SideOnly(Side.CLIENT)
-        public String getTranslatedTabLabel()
-        {
-            return StatCollector.translateToLocal("key.categories.ee3");
         }
     };
 }
