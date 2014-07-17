be80bcc17665f9bc82b65b9f194138578c7a35d1
Pahimar <pahimar@gmail.com>
2014-07-17 16:07:13 -0400

    More modes

--- src/main/java/com/pahimar/ee3/reference/ToolMode.java
+++ src/main/java/com/pahimar/ee3/reference/ToolMode.java
@@ -2,7 +2,7 @@ package com.pahimar.ee3.reference;
 
 public enum ToolMode
 {
-    STANDARD, DEEP, WIDE, TALL, SQUARE, CUBE;
+    STANDARD, DEEP, WIDE, TALL, SQUARE, CUBE, SLASH, MEGA;
 
     public static final ToolMode[] TYPES = ToolMode.values();
 }
