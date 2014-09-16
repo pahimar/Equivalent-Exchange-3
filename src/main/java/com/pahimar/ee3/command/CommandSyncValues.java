b3efb75ef8aa81f812601e04f1672947a542e61e
Pahimar <pahimar@gmail.com>
2014-09-16 12:38:28 -0400

    Add in a throttle for users syncing values while on a server to prevent massive amounts of network spam

--- src/main/java/com/pahimar/ee3/command/CommandSyncValues.java
+++ src/main/java/com/pahimar/ee3/command/CommandSyncValues.java
@@ -12,14 +12,26 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChatComponentTranslation;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class CommandSyncValues extends CommandBase
 {
+    private static final long SYNC_THRESHOLD = 5000;
+    private static Map<UUID, Long> requesterMap = new HashMap<UUID, Long>();
+
     @Override
     public String getCommandName()
     {
         return "ee3-sync-values";
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender commandSender)
+    {
+        return true;
     }
 
     @Override
@@ -35,15 +47,55 @@ public class CommandSyncValues extends CommandBase
         {
             if (args[0].equalsIgnoreCase("self"))
             {
-                LogHelper.info(String.format("Syncing EnergyValues with player '%s' at their request", commandSender.getCommandSenderName()));
-                PacketHandler.INSTANCE.sendTo(new MessageSyncEnergyValues(EnergyValueRegistry.getInstance()), (EntityPlayerMP) commandSender);
-                commandSender.addChatMessage(new ChatComponentTranslation("command.ee3.sync-values.self.success"));
+                boolean shouldSync = true;
+                float coolDown = 0f;
+
+                if (!PlayerHelper.isPlayerOp(((EntityPlayer) commandSender)))
+                {
+                    UUID commandSenderUUID = ((EntityPlayer) commandSender).getUniqueID();
+
+                    if (requesterMap.containsKey(commandSenderUUID))
+                    {
+                        long timeDifference = System.currentTimeMillis() - requesterMap.get(commandSenderUUID).longValue();
+                        if (timeDifference >= SYNC_THRESHOLD)
+                        {
+                            requesterMap.remove(commandSenderUUID);
+                        }
+                        else
+                        {
+                            coolDown = SYNC_THRESHOLD - timeDifference;
+                            shouldSync = false;
+                        }
+                    }
+                    else
+                    {
+                        requesterMap.put(commandSenderUUID, System.currentTimeMillis());
+                    }
+                }
+
+                if (shouldSync)
+                {
+                    LogHelper.info(String.format("Syncing EnergyValues with player '%s' at their request", commandSender.getCommandSenderName()));
+                    PacketHandler.INSTANCE.sendTo(new MessageSyncEnergyValues(EnergyValueRegistry.getInstance()), (EntityPlayerMP) commandSender);
+                    commandSender.addChatMessage(new ChatComponentTranslation("command.ee3.sync-values.self.success"));
+                }
+                else
+                {
+                    throw new WrongUsageException("command.ee3.sync-values.self.denied", new Object[]{coolDown / 1000f});
+                }
             }
-            else if (args[0].equalsIgnoreCase("all") && PlayerHelper.isPlayerOp((EntityPlayer) commandSender))
+            else if (args[0].equalsIgnoreCase("all"))
             {
-                LogHelper.info(String.format("Syncing EnergyValues with all players at %s's request", commandSender.getCommandSenderName()));
-                PacketHandler.INSTANCE.sendToAll(new MessageSyncEnergyValues(EnergyValueRegistry.getInstance()));
-                func_152373_a(commandSender, this, "command.ee3.sync-values.all.success", new Object[]{commandSender.getCommandSenderName()});
+                if (PlayerHelper.isPlayerOp((EntityPlayer) commandSender))
+                {
+                    LogHelper.info(String.format("Syncing EnergyValues with all players at %s's request", commandSender.getCommandSenderName()));
+                    PacketHandler.INSTANCE.sendToAll(new MessageSyncEnergyValues(EnergyValueRegistry.getInstance()));
+                    func_152373_a(commandSender, this, "command.ee3.sync-values.all.success", new Object[]{commandSender.getCommandSenderName()});
+                }
+                else
+                {
+                    throw new WrongUsageException("command.ee3.sync-values.all.denied");
+                }
             }
             else
             {
