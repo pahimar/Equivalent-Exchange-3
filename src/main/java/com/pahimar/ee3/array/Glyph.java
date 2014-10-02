731ec0fc777e4a0f874ac020351cee24c1ff8ec5
Pahimar <pahimar@gmail.com>
2014-10-02 00:16:47 -0400

    Some WAILA work, some Glyph work

--- src/main/java/com/pahimar/ee3/array/Glyph.java
+++ src/main/java/com/pahimar/ee3/array/Glyph.java
@@ -1,24 +1,24 @@
-package com.pahimar.ee3.alchemy;
+package com.pahimar.ee3.array;
 
 import com.pahimar.ee3.util.ResourceLocationHelper;
 import net.minecraft.util.ResourceLocation;
 
-public class Symbol
+public class Glyph
 {
     private final ResourceLocation texture;
     private final int size;
 
-    public Symbol(String texture)
+    public Glyph(String texture)
     {
         this(texture, 1);
     }
 
-    public Symbol(String texture, int size)
+    public Glyph(String texture, int size)
     {
         this(ResourceLocationHelper.getResourceLocation(texture), size);
     }
 
-    public Symbol(ResourceLocation texture, int size)
+    public Glyph(ResourceLocation texture, int size)
     {
         this.texture = texture;
         this.size = size;
@@ -37,9 +37,9 @@ public class Symbol
     @Override
     public boolean equals(Object object)
     {
-        if (object instanceof Symbol)
+        if (object instanceof Glyph)
         {
-            return this.texture.equals(((Symbol) object).getTexture()) && this.size == ((Symbol) object).size;
+            return this.texture.equals(((Glyph) object).getTexture()) && this.size == ((Glyph) object).size;
         }
 
         return false;
