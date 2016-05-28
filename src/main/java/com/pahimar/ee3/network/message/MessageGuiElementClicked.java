b083b0a73e48bf3ce8995b8f6b8790244733fd5e
Pahimar <pahimar@gmail.com>
2016-05-28 09:51:46 -0400

    Simplify logic statement

--- src/main/java/com/pahimar/ee3/network/message/MessageGuiElementClicked.java
+++ src/main/java/com/pahimar/ee3/network/message/MessageGuiElementClicked.java
@@ -7,49 +7,43 @@ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 
-public class MessageGuiElementClicked implements IMessage, IMessageHandler<MessageGuiElementClicked, IMessage>
-{
+public class MessageGuiElementClicked implements IMessage, IMessageHandler<MessageGuiElementClicked, IMessage> {
+
     public String elementName;
     public int buttonPressed;
 
-    public MessageGuiElementClicked()
-    {
-
+    public MessageGuiElementClicked(){
     }
 
-    public MessageGuiElementClicked(String elementName, int buttonPressed)
-    {
+    public MessageGuiElementClicked(String elementName, int buttonPressed) {
+
         this.elementName = elementName;
         this.buttonPressed = buttonPressed;
     }
 
     @Override
-    public void fromBytes(ByteBuf buf)
-    {
+    public void fromBytes(ByteBuf buf) {
+
         int elementNameLength = buf.readInt();
         this.elementName = new String(buf.readBytes(elementNameLength).array());
         this.buttonPressed = buf.readInt();
     }
 
     @Override
-    public void toBytes(ByteBuf buf)
-    {
+    public void toBytes(ByteBuf buf) {
+
         buf.writeInt(elementName.length());
         buf.writeBytes(elementName.getBytes());
         buf.writeInt(buttonPressed);
     }
 
     @Override
-    public IMessage onMessage(MessageGuiElementClicked message, MessageContext ctx)
-    {
+    public IMessage onMessage(MessageGuiElementClicked message, MessageContext ctx) {
+
         EntityPlayer entityPlayer = ctx.getServerHandler().playerEntity;
 
-        if (entityPlayer != null)
-        {
-            if (entityPlayer.openContainer instanceof IElementButtonHandler)
-            {
-                ((IElementButtonHandler) entityPlayer.openContainer).handleElementButtonClick(message.elementName, message.buttonPressed);
-            }
+        if (entityPlayer != null && entityPlayer.openContainer instanceof IElementButtonHandler) {
+            ((IElementButtonHandler) entityPlayer.openContainer).handleElementButtonClick(message.elementName, message.buttonPressed);
         }
 
         return null;
