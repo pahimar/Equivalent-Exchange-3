bdb4a367af773a69be0d74e6631ea8588b594dbf
Pahimar <pahimar@gmail.com>
2016-06-01 16:02:06 -0400

    In ForgeGradle 2.2 I hear we can use lambdas with reobf names, so switching how we add "vanilla" recipes to the RecipeRegistry to use streams (cleaner code)

--- src/main/java/com/pahimar/ee3/recipe/RecipesVanilla.java
+++ src/main/java/com/pahimar/ee3/recipe/RecipesVanilla.java
@@ -7,39 +7,30 @@ import com.pahimar.ee3.util.RecipeHelper;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 
 public class RecipesVanilla {
 
     public void registerRecipes() {
 
-        for (Object recipeObject : CraftingManager.getInstance().getRecipeList()) {
-
-            // Vanilla
-            if (recipeObject instanceof ShapedRecipes || recipeObject instanceof ShapelessRecipes || recipeObject instanceof ShapedOreRecipe || recipeObject instanceof ShapelessOreRecipe) {
-                IRecipe recipe = (IRecipe) recipeObject;
-                ItemStack recipeOutput = recipe.getRecipeOutput();
-
-                if (recipeOutput != null) {
-
-                    List<WrappedStack> recipeInputs = RecipeHelper.getRecipeInputs(recipe);
+        CraftingManager.getInstance().getRecipeList().stream()
+                .filter(iRecipe -> iRecipe instanceof ShapedRecipes || iRecipe instanceof ShapelessRecipes || iRecipe instanceof ShapedOreRecipe || iRecipe instanceof ShapelessOreRecipe)
+                .filter(iRecipe -> iRecipe.getRecipeOutput() != null)
+                .forEach(iRecipe -> {
+                    Set<WrappedStack> recipeInputs = RecipeHelper.getRecipeInputs(iRecipe);
 
                     if (!recipeInputs.isEmpty()) {
-                        RecipeRegistryProxy.addRecipe(recipeOutput, recipeInputs);
+                        RecipeRegistryProxy.addRecipe(iRecipe.getRecipeOutput(), recipeInputs);
                     }
-                }
-            }
-        }
+        });
 
         // Fixes for OreDictionary entries that may not exist (because the OreDictionary entry has nothing in it)
-        RecipeRegistryProxy.addRecipe(new ItemStack(Blocks.daylight_detector), Arrays.asList(new ItemStack(Blocks.glass, 3, OreDictionary.WILDCARD_VALUE), new OreStack("slabWood", 3), new OreStack("gemQuartz", 3)));
+        RecipeRegistryProxy.addRecipe(new ItemStack(Blocks.DAYLIGHT_DETECTOR), new ItemStack(Blocks.GLASS, 3, OreDictionary.WILDCARD_VALUE), new OreStack("slabWood", 3), new OreStack("gemQuartz", 3));
     }
 }
