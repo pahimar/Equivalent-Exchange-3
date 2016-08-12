4fcaca8c6f4f1f37ef8d76e5d435bd2db1434257
pahimar <pahimar@gmail.com>
2016-08-11 22:33:44 -0400

    Bump minimum Forge version required to 12.18.1.*

--- src/main/java/com/pahimar/ee3/EquivalentExchange3.java
+++ src/main/java/com/pahimar/ee3/EquivalentExchange3.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
         name = "Equivalent Exchange 3",
         certificateFingerprint = EquivalentExchange3.FINGERPRINT,
         version = "@MOD_VERSION@",
-        dependencies = "required-after:Forge@[12.17.0,)",
+        dependencies = "required-after:Forge@[12.18.1,)",
         guiFactory = "com.pahimar.ee3.client.gui.GuiFactory",
         updateJSON = "http://ee3.pahimar.com/update/update.json")
 public class EquivalentExchange3 {
