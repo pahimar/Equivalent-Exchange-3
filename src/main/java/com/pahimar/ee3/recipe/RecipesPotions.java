08f6824fb86da9d34a2ee8e6b3d9bdc26aae0dc5
pahimar <pahimar@gmail.com>
2016-08-11 22:35:24 -0400

    Fix the recipe inputs passed into the recipe registry to tell it how lingering potions of poison (long) are created

--- src/main/java/com/pahimar/ee3/recipe/RecipesPotions.java
+++ src/main/java/com/pahimar/ee3/recipe/RecipesPotions.java
@@ -365,7 +365,7 @@ public class RecipesPotions {
         RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_HARMING_STRONG, 3), ItemStackUtils.clone(LINGERING_POTION_POISON_STRONG, 3), REAGENT_FERMENTED_SPIDER_EYE);
         RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_POISON, 3), ItemStackUtils.clone(SPLASH_POTION_POISON, 3), REAGENT_DRAGONS_BREATH);
         RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_POISON, 3), ItemStackUtils.clone(LINGERING_POTION_AWKWARD, 3), REAGENT_SPIDER_EYE);
-        RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_POISON_LONG, 3), ItemStackUtils.clone(SPLASH_POTION_POISON_LONG, 3), REAGENT_SPIDER_EYE);
+        RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_POISON_LONG, 3), ItemStackUtils.clone(SPLASH_POTION_POISON_LONG, 3), REAGENT_DRAGONS_BREATH);
         RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_POISON_LONG, 3), ItemStackUtils.clone(LINGERING_POTION_POISON_LONG, 3), REAGENT_REDSTONE_DUST);
         RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_POISON_STRONG, 3), ItemStackUtils.clone(LINGERING_POTION_POISON, 3), REAGENT_GLOWSTONE_DUST);
         RecipeRegistryProxy.addRecipe(ItemStackUtils.clone(LINGERING_POTION_POISON_STRONG, 3), ItemStackUtils.clone(SPLASH_POTION_POISON_STRONG, 3), REAGENT_DRAGONS_BREATH);
