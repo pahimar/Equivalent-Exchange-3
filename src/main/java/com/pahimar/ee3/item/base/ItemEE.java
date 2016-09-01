fa2634d5bae711134f1836a25dcace9f59dbe71d
Pahimar <pahimar@gmail.com>
2016-09-01 11:29:12 -0400

    Rework how items have their models and names read in - simplifies a lot of things

--- src/main/java/com/pahimar/ee3/item/base/ItemEE.java
+++ src/main/java/com/pahimar/ee3/item/base/ItemEE.java
@@ -13,6 +13,7 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemEE extends Item implements IItemVariantHolder<ItemEE> {
@@ -29,11 +30,11 @@ public class ItemEE extends Item implements IItemVariantHolder<ItemEE> {
         setNoRepair();
 
         BASE_NAME = name;
-        if (variants.length == 0) {
-            VARIANTS = new String[] { "normal" };
+        if (variants.length > 0) {
+            VARIANTS = variants;
         }
         else {
-            VARIANTS = variants;
+            VARIANTS = new String[0];
         }
 
         ModItems.ITEMS.add(this);
@@ -43,44 +44,52 @@ public class ItemEE extends Item implements IItemVariantHolder<ItemEE> {
     public String getUnlocalizedName(ItemStack itemStack) {
 
         if (hasSubtypes && itemStack.getMetadata() < VARIANTS.length) {
-            return String.format("item.%s", VARIANTS[itemStack.getMetadata()]);
+            return String.format("item.%s:%s", EquivalentExchange3.MOD_ID, VARIANTS[itemStack.getMetadata()]);
         }
-        return super.getUnlocalizedName(itemStack);
+        else {
+            return String.format("item.%s:%s", EquivalentExchange3.MOD_ID, BASE_NAME);
+        }
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public void getSubItems(Item item, CreativeTabs creativeTab, List<ItemStack> list) {
 
-        if (!getHasSubtypes()) {
-            super.getSubItems(item, creativeTab, list);
-        }
-        else {
+        if (getHasSubtypes() && VARIANTS.length > 0) {
             for (int meta = 0; meta < VARIANTS.length; ++meta) {
                 list.add(new ItemStack(this, 1, meta));
             }
+        }
+        else {
+            super.getSubItems(item, creativeTab, list);
         }
     }
 
     @SideOnly(Side.CLIENT)
     public void initModelsAndVariants() {
 
-        // TODO TRACE level logging
         if (getCustomMeshDefinition() != null) {
 
-            ModelLoader.setCustomMeshDefinition(this, getCustomMeshDefinition());
             for (int i = 0; i < VARIANTS.length; i++) {
                 ModelBakery.registerItemVariants(this, getCustomModelResourceLocation(VARIANTS[i]));
             }
+
+            ModelLoader.setCustomMeshDefinition(this, getCustomMeshDefinition());
         }
         else {
-            if (!getHasSubtypes()) {
-                ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName().toString()));
+
+            if (getHasSubtypes() && VARIANTS.length > 0) {
+                List<ModelResourceLocation> modelResources = new ArrayList<>();
+
+                for (int i = 0; i < VARIANTS.length; i++) {
+                    modelResources.add(getCustomModelResourceLocation(VARIANTS[i]));
+                }
+
+                ModelBakery.registerItemVariants(this, modelResources.toArray(new ModelResourceLocation[0]));
+                ModelLoader.setCustomMeshDefinition(this, itemStack -> modelResources.get(itemStack.getMetadata()));
             }
             else {
-                for (int i = 0; i < VARIANTS.length; i++) {
-                    ModelLoader.setCustomModelResourceLocation(this, i, getCustomModelResourceLocation(VARIANTS[i]));
-                }
+                ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName().toString()));
             }
         }
     }
@@ -100,7 +109,7 @@ public class ItemEE extends Item implements IItemVariantHolder<ItemEE> {
         return null;
     }
 
-    protected ModelResourceLocation getCustomModelResourceLocation(String variant) {
+    private ModelResourceLocation getCustomModelResourceLocation(String variant) {
         return new ModelResourceLocation(EquivalentExchange3.MOD_ID + ":" + variant);
     }
 }
