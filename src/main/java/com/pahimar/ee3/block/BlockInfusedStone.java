6a251390e48ada74db3a1bcf6e287f075a3c6858
Pahimar <pahimar@gmail.com>
2016-10-13 22:36:34 -0400

    Stub in some infused stone stuff

---
+++ src/main/java/com/pahimar/ee3/block/BlockInfusedStone.java
@@ -0,0 +1,10 @@
+package com.pahimar.ee3.block;
+
+import com.pahimar.ee3.block.base.BlockBase;
+
+public class BlockInfusedStone extends BlockBase {
+
+    public BlockInfusedStone() {
+        super("infused_stone");
+    }
+}
---
+++ src/main/java/com/pahimar/ee3/block/BlockInfusedStoneSlab.java
@@ -0,0 +1,10 @@
+package com.pahimar.ee3.block;
+
+import com.pahimar.ee3.block.base.BlockBase;
+
+public class BlockInfusedStoneSlab extends BlockBase {
+
+    public BlockInfusedStoneSlab() {
+        super("infused_stone_slab");
+    }
+}
