120ad367fc3dd5f2db495f8d1de2238aa31c294f
Pahimar <pahimar@gmail.com>
2016-11-15 20:55:44 -0500

    Bump Forge to 2125 and update MCP names

--- src/main/java/com/pahimar/ee3/block/BlockInfusedStoneSlab.java
+++ src/main/java/com/pahimar/ee3/block/BlockInfusedStoneSlab.java
@@ -1,10 +1,40 @@
 package com.pahimar.ee3.block;
 
 import com.pahimar.ee3.block.base.BlockBase;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockInfusedStoneSlab extends BlockBase {
 
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
+
     public BlockInfusedStoneSlab() {
         super("infused_stone_slab");
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.UP));
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING);
+    }
+
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState().withProperty(FACING, facing);
     }
 }
